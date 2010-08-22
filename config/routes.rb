7d82b15e98a2d54fa09f87a38924e21bb4ad0e43
Katsuyoshi Ito <kito@itosoft.com>
2010-08-22 19:17:00 +0900

    loginが衝突するので名称変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
-  map.login '/login', :controller => 'sessions', :action => 'new'
+  map.login '/user_login', :controller => 'sessions', :action => 'new'
   map.register '/register', :controller => 'users', :action => 'create'
   map.signup '/signup', :controller => 'users', :action => 'new'
   map.resources :users
