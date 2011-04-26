e53647490b7270706792ee1caf145278c461bfda
Katsuyoshi Ito <kito@itosoft.com>
2011-04-26 17:13:04 +0900

    新システムに合わせて変更

--- public/viewer/js/custMap.js
+++ public/viewer/js/custMap.js
@@ -1,22 +1,18 @@
 // JavaScript Document
 
 var mapInstance;
-var jsonFile = "/omatsuri/kakunodate/locations.json";
+//var jsonFile = "/omatsuri/kakunodate/locations.json";
+var jsonFile = "http://hikiyama-map.heroku.com/omatsuri/id:10/locations.json?jsoncallback=?";
 
 var clat = 39.59929722;//初期表示緯度
 var clong = 140.56142532;//初期表示経度
 var mapZoom = 15;//初期表示縮尺
 var rntime = 60;//自動更新間隔（秒）
 
-// 角館
-var defaultLocation = new google.maps.LatLng(39.592891, 140.570326);
-var halfDeltaLat = 0.016667 / 2.0;
-var halfDeltaLng = 0.034161 / 2.0;
-
-var swLat = defaultLocation.lat() - halfDeltaLat;
-var swLng = defaultLocation.lng() - halfDeltaLng;
-var neLat = defaultLocation.lat() + halfDeltaLat;
-var neLng = defaultLocation.lng() + halfDeltaLng;
+var swLat = 0;
+var swLng = 0;
+var neLat = 0;
+var neLng = 0;
 
 var mylat = 0;
 var mylng = 0;
@@ -35,14 +31,26 @@ var centerMode = 0;//1：現在地が動いたら中心も移動
 
 var posData = new Array();
 
+
+
 function getMarkerLocation(){
-	$.getJSON(jsonFile,{},function(data){
+/*	$.getJSON(jsonFile, {}, function(data) {
+		setMarker(data);
+	});*/
+	
+//	var dataURL = "http://hikiyama-map.heroku.com/omatsuri/" +
+//				OmatsuriId + "/" + YamaNameAr[i] + "/locations/" + 
+//				QueryStrs.sd + "/" + QueryStrs.ed + ".json" + "?jsoncallback=?";
+	$.getJSON(jsonFile, {}, function(data){
+		//My.onTrackingDataLoaded(data);	// ※直接onTrackingDataLoaded()をイベントハンドラーにするとthisがインスタンスを参照しないので不都合。
 		setMarker(data);
 	});
 }
 
-function setMarker(json){
 
+
+
+function setMarker(json){
 	iniFlg = 0;
 	strOption = "";
 	
@@ -91,19 +99,19 @@ function setMarker(json){
 			}
 			addMarker(lat,lng,heading,hid,ts,id,ho_a,head_a,h_name,icons);	
 			strOption += '<option value="' + lat + ',' + lng + '">' + h_name + '</option>'
-
 		}
-		
 	}
-
+	
 	if (ini==0){
 		fitMap();
 		ini = 1;
 	}
-
+	
 	$("#slIt").html('<select id="fllist"><option value="">◆中心に移動</option>' + strOption + '</select>');
-
 }
+
+
+
 function nowTime(){
 	var now = new Date();
 
@@ -129,9 +137,11 @@ function makeIconURL(def,hid,heading,icons){
 }
 
 
+
 function addMarker(lat,lng,heading,hid,ts,id,ho_a,head_a,h_name,icons){
 
-	var mUrl = makeIconURL(defIcon,hid,heading,icons);
+//	var mUrl = makeIconURL(defIcon,hid,heading,icons);
+	var mUrl = "http://www.hikiyama-map.com/taguchi/image/sakura.png";
 	var mSize = new google.maps.Size(40, 40);
 	var mOrigin = new google.maps.Point(0,0);
 	var mAnchor = new google.maps.Point(20,20);
@@ -160,8 +170,9 @@ function addMarker(lat,lng,heading,hid,ts,id,ho_a,head_a,h_name,icons){
 	google.maps.event.addListener(marker,"click",function(){
 		info.open(mapInstance,marker);
 	});
-
 }
+
+
 
 function fitMap(){
   var southWest = new google.maps.LatLng(swLat,swLng);
@@ -178,12 +189,14 @@ function deleteAllMarker(mList){
 		marker.setMap(null);
 	});
 }
+
 function popMVCList(mList){
 	mlLength = mList.getLength();
  	for(i=0;i<mlLength;i++){
 		mList.pop();
 	}
 }
+
 function makeMyPos(lat,lng,acc){
 	mylat = lat;
 	mylng = lng;
@@ -193,7 +206,7 @@ function makeMyPos(lat,lng,acc){
 	var mOrigin = new google.maps.Point(0,0);
 	var mAnchor = new google.maps.Point(16,32);
 	var mScaleSize = new google.maps.Size(32, 32);
-	var mIcon = new google.maps.MarkerImage(mUrl,mSize,mOrigin,mAnchor,mScaleSize); 	
+	var mIcon = new google.maps.MarkerImage(mUrl,mSize,mOrigin,mAnchor,mScaleSize); 
 	if(nowPosMarker){
 		nowPosMarker.setMap(null);
 	}
@@ -212,10 +225,13 @@ function makeMyPos(lat,lng,acc){
 	}
 }
 
+
+
 function setCenter(lat,lng){
 	var posNow = new google.maps.LatLng(lat, lng);
 	mapInstance.setCenter(posNow);
 }
+
 
 
 google.maps.event.addDomListener(window, 'load', function() {
